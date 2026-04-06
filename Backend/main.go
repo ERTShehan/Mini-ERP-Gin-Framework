3ea48d952169dc564a93c064fff53640bc20f96e
Shehan <ertsshehan@gmail.com>
2026-04-06 14:35:08 +0530

    initialize main application with database setup and route configuration

--- Backend/main.go
+++ Backend/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"mini-erp/config"
+	"mini-erp/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	config.InitDB()
+	r := gin.Default()
+	routes.SetupRoutes(r)
+	r.Run(":8080")
+}
