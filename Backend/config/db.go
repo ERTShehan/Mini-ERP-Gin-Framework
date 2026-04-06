8b5de6f001dcc49e97cbd21246d5f2c4c4519781
Shehan <ertsshehan@gmail.com>
2026-04-06 14:10:48 +0530

    add database configuration and initialization in db.go; create main.go file

---
+++ Backend/config/db.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func InitDB() {
+	var err error
+	DB, err = sql.Open("mysql", "root:Ijse@1234@tcp(127.0.0.1:3306)/mini_erp")
+	if err != nil {
+		panic(err)
+	}
+}
