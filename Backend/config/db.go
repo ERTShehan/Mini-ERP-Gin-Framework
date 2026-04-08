fb8a6346dfb2b63b5a21e020832eb41305217346
Shehan <ertsshehan@gmail.com>
2026-04-08 20:52:51 +0530

    integrate godotenv for environment variable management in database configuration

--- Backend/config/db.go
+++ Backend/config/db.go
@@ -2,15 +2,24 @@ package config
 
 import (
 	"database/sql"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
 func InitDB() {
 	var err error
-	DB, err = sql.Open("mysql", "root:Ijse@1234@tcp(127.0.0.1:3306)/minierp")
+	
+	godotenv.Load()
+
+	dsn := os.Getenv("DB_DSN")
+	if dsn == "" {
+		dsn = "root:Ijse@1234@tcp(127.0.0.1:3306)/minierp"
+	}
+	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
 		panic(err)
 	}
